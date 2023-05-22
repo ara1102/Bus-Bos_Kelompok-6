91e2374675250c6c2c17e9c45aa2b1e218d8189c
nyawnayaw <hglenaya@gmail.com>
2023-05-23 01:40:28 +0700

    heuristic udh gan

--- Backend/flask-server/heuristic.py
+++ Backend/flask-server/heuristic.py
@@ -19,19 +19,110 @@ edges = [
 # BN J WJ CJ Y EJ (buat kode banten, jakarta, central java, yogyakarta, east java)
 # buat nilai heuristicnya ibunya bilang kemaren coba pake straight line distancenya(tp blm tentu fix bisa), tp kita juga bisa aja ngebuat nilai heuristicnya sendiri
 
-# BN_heuristic_time = {
-#     'Jawa Timur' : 0,
-#     'Jawa Tengah' : 0,
-#     'Jawa Barat' : 0,
-#     'Banten' : 0,
-#     'DKI Jakarta' : 0,
-#     'Yogyakarta' : 0,
+# Serang_heuristic_distance = {
+#     'Surabaya' : 800,
+#     'Semarang' : 493,
+#     'Bandung' : 140,
+#     'Serang' : 0,
+#     'Jakarta Pusat' : 60,
+#     'Jogja' : 537,
 # }
-# BN_heuristic_distance = {
-#     'Jawa Timur' : 0,
-#     'Jawa Tengah' : 0,
-#     'Jawa Barat' : 0,
-#     'Banten' : 0,
-#     'DKI Jakarta' : 0,
-#     'Yogyakarta' : 0,
+# Serang_heuristic_time = {
+#     'Serang' : 0,
+#     'Jakarta Pusat' : 70,
+#     'Bandung' : 210,
+#     'Semarang' : 400,
+#     'Jogja' : 545,
+#     'Surabaya' : 650,
 # }
+
+JakartaPusat_heuristic_distance = {
+    'Serang' : 60,
+    'Jakarta Pusat' : 0,
+    'Bandung' : 75,
+    'Semarang' : 426,
+    'Jogja' : 465,
+    'Surabaya' : 752,
+}
+
+JakartaPusat_heuristic_time = {
+    'Serang' : 70,
+    'Jakarta Pusat' : 0,
+    'Bandung' : 139,
+    'Semarang' : 325,
+    'Jogja' : 490,
+    'Surabaya' : 570,
+}
+
+Bandung_heuristic_distance = {
+    'Serang' : 140,
+    'Jakarta Pusat' : 75,
+    'Bandung' : 0,
+    'Semarang' : 345,
+    'Jogja' : 390,
+    'Surabaya' : 676,
+}
+
+Bandung_heuristic_time = {
+    'Serang' : 210,
+    'Jakarta Pusat' : 139,
+    'Bandung' : 0,
+    'Semarang' : 308,
+    'Jogja' : 435,
+    'Surabaya' : 560,
+}
+
+Semarang_heuristic_distance = {
+    'Serang' : 493,
+    'Jakarta Pusat' : 426,
+    'Bandung' : 345,
+    'Semarang' : 0,
+    'Jogja' : 125,
+    'Surabaya' : 324,
+}
+
+Semarang_heuristic_time = {
+    'Serang' : 400,
+    'Jakarta Pusat' : 325,
+    'Bandung' : 308,
+    'Semarang' : 0,
+    'Jogja' : 159,
+    'Surabaya' : 252,
+}
+
+Jogja_heuristic_distance = {
+    'Serang' : 537,
+    'Jakarta Pusat' : 465,
+    'Bandung' : 390,
+    'Semarang' : 125,
+    'Jogja' : 0,
+    'Surabaya' : 316,
+}
+
+Jogja_heuristic_time = {
+    'Serang' : 545,
+    'Jakarta Pusat' : 490,
+    'Bandung' : 435,
+    'Semarang' : 159,
+    'Jogja' : 0,
+    'Surabaya' : 287,
+}
+
+Surabaya_heuristic_distance = {
+    'Serang' : 800,
+    'Jakarta Pusat' : 752,
+    'Bandung' : 676,
+    'Semarang' : 324,
+    'Jogja' : 316,
+    'Surabaya' : 0,
+}
+
+Surabaya_heuristic_time = {
+    'Serang' : 650,
+    'Jakarta Pusat' : 570,
+    'Bandung' : 560,
+    'Semarang' : 252,
+    'Jogja' : 287,
+    'Surabaya' : 0,
+}
+
